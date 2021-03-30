de9e9dd4801089f63087db023e1ec770c65a7438
Barcoaie <florinel.barcan@gmail.com>
2021-03-30 15:14:25 +0300

    ran 2 tests

--- src/test/java/ssvv/barcanbatincu/AppTest.java
+++ src/test/java/ssvv/barcanbatincu/AppTest.java
@@ -3,6 +3,10 @@ package ssvv.barcanbatincu;
 import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
+import ssvv.barcanbatincu.domain.TemaLab;
+import ssvv.barcanbatincu.exceptions.ValidatorException;
+import ssvv.barcanbatincu.repo.TemaLabRepo;
+import ssvv.barcanbatincu.validator.TemaLabValidator;
 
 /**
  * Unit test for simple App.
@@ -16,5 +20,31 @@ public class AppTest
     public void shouldAnswerWithTrue()
     {
         assertTrue( true );
+    }
+
+    @Test
+    public void addAssignmentTest1() {
+        TemaLabValidator temaLabValidator = new TemaLabValidator();
+        TemaLabRepo temaLabRepo = new TemaLabRepo(temaLabValidator);
+
+        try {
+            temaLabRepo.save(new TemaLab(1, "a fost si nu va mai fi", 2, 3));
+            assert temaLabRepo.size() == 1;
+        } catch (ValidatorException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void addAssignmentTest2() {
+        TemaLabValidator temaLabValidator = new TemaLabValidator();
+        TemaLabRepo temaLabRepo = new TemaLabRepo(temaLabValidator);
+        try {
+            temaLabRepo.save(null);
+            assert temaLabRepo.size() == 0;
+        } catch (Exception e) {
+            assert e.toString().equals("java.lang.IllegalArgumentException: Entity can not be null!\n");
+            e.printStackTrace();
+        }
     }
 }
